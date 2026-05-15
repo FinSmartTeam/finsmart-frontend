b9b616529de54e307b6b23180bbd361d5bd86adb
antawirangga <antaid.official@gmail.com>
2026-05-15 18:18:10 +0700

    feat: update package & app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
 
 function App() {
-  // status: 'landing' | 'auth' | 'dashboard'
-  const [currentPage, setCurrentPage] = useState('landing');
-
   return (
-    <div className="min-h-screen bg-base-dark">
-      {currentPage === 'landing' && (
-        <LandingPage onStart={() => setCurrentPage('auth')} />
-      )}
-
-      {currentPage === 'auth' && (
-        <Auth onAuthSuccess={() => setCurrentPage('dashboard')} />
-      )}
-
-      {currentPage === 'dashboard' && (
-        <Dashboard />
-      )}
-    </div>
+    <Router>
+      <div className="min-h-screen bg-base-dark">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
