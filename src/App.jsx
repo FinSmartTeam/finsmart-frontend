1305e1b96fcca4b2e79b033fee99e85da191dcfb
antawirangga <antaid.official@gmail.com>
2026-05-16 09:59:15 +0700

    feat(security): implement protected route to secure dashboard access

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
-
+import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-base-dark">
         <Routes>
+          {/* Rute Publik */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* Rute Protected */}
+          <Route 
+            path="/dashboard" 
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } 
+          />
         </Routes>
       </div>
     </Router>
